beeba1691f3e1f474e3da6ad597b35e3cd763b1d
Valentin Rutz <valentin.rutz@epfl.ch>
2015-09-29 16:32:17 +0200

    find uses topDownBreak and finds the exact number of calls

--- tests/src/test/scala/Test.scala
+++ tests/src/test/scala/Test.scala
@@ -5,16 +5,13 @@ import scala.meta.dialects.Scala211
 
 class TestSuite extends FunSuite {
 	test("foo") {
-		def find(toFind: String)(tree: Tree): Unit = tree.collect {
-			case t @ q"$expr.$name" if name.toString == toFind => println(s"Found one $toFind in expression: $t")
-			case t: Term.Name if t.toString == toFind => println(s"Found one $toFind in expression: $t")
+		def find(toFind: String)(tree: Tree): Unit = tree.topDownBreak.collect {
+			case t @ q"$expr.$name" if name.toString == toFind =>
+				println(s"Found one $toFind in expression: $t")
+				find(toFind)(expr)
+			case t: Term.Name if t.toString == toFind =>
+				println(s"Found one $toFind in expression: $t")
 		}
-/*
-		tree \\: visit[Set] {
-			case t @ q"$expr.$name" if name.toString == toFind => println(s"Found one get in expression: $t"); Set(expr)
-			case t: Term.Name if t.toString == toFind => println(s"Found one get in expression: $t"); Set.empty
-		} \\: feed { _.map(expr: Tree => find(toFind)(expr)) }
-*/
 		find("get")("""
 			|object O {
 			|	def main(args: Array[String]) {
