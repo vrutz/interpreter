10a0527795753560fa3aa3e5c64a5dedc051c107
Valentin Rutz <valentin.rutz@epfl.ch>
2015-10-07 19:10:32 +0200

    first simplified try at replacing an infix operator by its evaluated code

--- interpreter/src/main/scala/interpreter/Interpreter.scala
+++ interpreter/src/main/scala/interpreter/Interpreter.scala
@@ -10,11 +10,32 @@ object Interpreter {
   }
 
   def evaluate(term: Tree, env: Environment)(implicit ctx: Context): (Value, Environment) = term match {
-    case q"$expr $name[..$tpes] (..$aexprs)" =>
+    case q"${expr: Tree} ${name: Term.Name}[..$tpes] (..$aexprs)" =>
+      val justArgExprs: Seq[Tree] = aexprs map {
+        case arg"$name = $expr" => expr
+        case arg"$expr: _*" => expr
+        case arg"$expr" => expr
+      }
       val (ev1,  env1) = evaluate(expr, env)
-      val (ev2s: Seq[Value], env2: Environment) = evaluate(aexprs, env1)
+      val (ev2s: Seq[Value], env2: Environment) = evaluate(aexprs: Seq[Term.Arg], env1)
       name.defn match {
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" => println(paramss)
+        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" =>
+          val p: Seq[(Term.Param.Name, Option[Tree])] = (paramss: Seq[Seq[Term.Param]]).flatten map {
+              case param"..$mods $paramname: $atpeopt = $expropt" => (paramname: Term.Param.Name, expropt: Option[Tree])
+            }
+
+          val defParamsExprs: Seq[Tree] = p.unzip._2.takeRight(p.length - ev2s.length).map(_.get)
+
+          val paramsWithExpr = (p.unzip._1 zip (justArgExprs ++ defParamsExprs)).toMap
+
+          val substExpr: Tree = ((expr: Tree) transform {
+              case name: Term.Name if paramsWithExpr.exists { case (n: Term.Param.Name, _) => n.toString == name.toString } =>
+                paramsWithExpr(name)
+              case q"this" => expr
+            }).asInstanceOf[Tree]
+
+          (evaluate(substExpr, env2)._1, env2)
+
         case _ => println("Should not happen")
       }
       // Replace type parameters in body of the function with tpes
