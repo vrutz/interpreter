79733b9b315fef490d2940a93e7cca51acb4e090
Valentin Rutz <valentin.rutz@epfl.ch>
2015-10-14 19:54:25 +0200

    impossible to test because of the  Pattern->Term.Name deconstruction error

--- interpreter/src/main/scala/interpreter/Interpreter.scala
+++ interpreter/src/main/scala/interpreter/Interpreter.scala
@@ -30,17 +30,18 @@ object Interpreter {
       /* Expressions */
     // this: this | <expr>.this
     case q"this" => (env(This), env)
-//    case q"${qname: Tree}.this" => evaluate(qname, env)
+//    case q"${qname: Term}.this" => evaluate(qname, env)
 
+    // TODO Scala meta bug
     // super: super | <expr>.super | super[<expr>] | <expr>.super[<expr]
 //    case q"super" => (env(Super), env)
-//    case q"${qname: Tree}.super" =>
+//    case q"${qname: Term}.super" =>
 //      val (instance: Instance, instanceEnv: Environment) = evaluate(qname, env)
 //      (instanceEnv(Super), instanceEnv)
-//    case q"super[${qname: Tree}]" =>
+//    case q"super[${qname: Term}]" =>
 //      val (typeToApp: interpreter.Type, typeEnv) = evaluate(qname, env)
 //      (env(Super), typeEnv)
-//    case q"${qname0: Tree}.super[${qname1: Tree}]" =>
+//    case q"${qname0: Term}.super[${qname1: Term}]" =>
 //      val (caller: Instance, callerEnv: Environment) = evaluate(qname0, env)
 //      (callerEnv(Super), callerEnv)
 
@@ -111,21 +112,55 @@ object Interpreter {
     case q"{ ..$stats}" =>
       val lastFrame: Frame = env.get
       val blockEnv = env push lastFrame
-      stats.foldLeft((List[Value](), blockEnv)) {
-        case ((evaluatedExprs, newEnv), nextExpr) =>
+      val (l: List[Value], newEnv: Environment) = stats.foldLeft((List[Value](), blockEnv)) {
+        case ((evaluatedExprs, exprEnv), nextExpr) =>
+          println(nextExpr)
           nextExpr match {
-//            case q"${expr: Term}" => evaluate(expr, newEnv)
-            case q"..$mods val ..$pats: $tpeopt = $expr" => pats map {
-                case p"$name" =>
-                case p"(..$pats)" =>
-                case p"$ref(..$apats)" =>
-              }
-            case q"..$mods var ..$pats: $tpeopt = $expropt" =>
+            // TODO Scala meta bug
+//            case q"${expr: Term}" => evaluate(expr, exprEnv)
+            case q"..$mods val ..$pats: $tpeopt = $expr" => (Literal(()) :: evaluatedExprs, link(pats, expr, exprEnv))
+            case q"..$mods var ..$pats: $tpeopt = $expropt" => ???
           }
           ???
       }
-      ???
+      (l.head, newEnv)
     case t => (Literal(null), env)
+  }
+
+  def link(pats: Seq[Pat], expr: Term, env: Environment)(implicit c: Context): Environment = {
+    pats.foldLeft(env) {
+      case (newEnv, p"_") => evaluate(expr, newEnv)._2
+
+      // TODO Scala meta bug
+//      case p"${name: Term.Name}" =>
+//        val (evaluatedExpr: Value, exprEnv) = evaluate(expr, env)
+//        exprEnv + (Local(name), evaluatedExpr)
+
+      case (newEnv, p"(..$pats0)") => expr match {
+        case q"(..$exprs)" => (pats0 zip exprs).foldLeft(newEnv) {
+          case (newNewEnv: Environment, (pat, e)) => link(Seq(pat), e, newNewEnv)
+        }
+      }
+
+      case (newEnv, p"$ref(..$apats)") =>
+        val justArgExprs = expr match {
+          case q"$expr0(..$aexprs)" =>
+            aexprs map {
+              case arg"$name = $expr0" => expr0
+              case arg"$expr0: _*" => expr0
+              case arg"$expr0" => expr0
+            }
+        }
+        val justPats = apats map {
+          case parg"$pat" => Seq(pat)
+//          case parg"_*" => p"_"
+        }
+        // TODO Scala meta bug
+//        (justPats zip justArgExprs).foldLeft(newEnv) {
+//          case (newNewEnv: Environment, (seqPat, e: Term)) => link(seqPat, e, newNewEnv)
+//        }
+        ???
+    }
   }
 
 	def find(toFind: String)(tree: Tree): Unit = tree.topDownBreak.collect {
--- interpreter/src/main/scala/interpreter/Slot.scala
+++ interpreter/src/main/scala/interpreter/Slot.scala
@@ -7,6 +7,12 @@ package interpreter
 import scala.meta._
 
 sealed trait Slot
-final case class Local(name: Term.Name) extends Slot
+
 case object This extends Slot
 case object Super extends Slot
+
+sealed trait Name extends Slot
+
+case object WildCard extends Name
+final case class Local(name: Term.Name) extends Name
+final case class Tuple(members: Name*) extends Name
